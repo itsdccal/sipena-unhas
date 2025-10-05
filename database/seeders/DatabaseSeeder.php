2d0c77928a1c72f8fe185e47136fb749d432626d
andi muh haikal lukman <113303662+itsdccal@users.noreply.github.com>
2025-10-05 20:27:50 +0800

    feat : database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,8 @@ class DatabaseSeeder extends Seeder
             DegreeSeeder::class,
             StudyProgramSeeder::class,
             UsersSeeder::class,
+            SemesterSeeder::class,
+            UnitSeeder::class,
         ]);
     }
 }
